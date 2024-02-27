3f6a238331fb541c6ea519b048dbcb0a820ad30f
iliaazarashv_er250wc <i.azarashvili@extra.ge>
2024-02-27 23:23:30 +0400

    sada

---
+++ pythonProject/book.py
@@ -0,0 +1,6 @@
+class Book:
+    def __init__(self, title, author, date):
+        self.title = title
+        self.author = author
+        self.date = date
+        
---
+++ pythonProject/book_manager.py
@@ -0,0 +1,13 @@
+class BookManager:
+
+    def __init__(self):
+        pass
+
+    def add_new_book(self):
+        pass
+
+    def print_all_books(self):
+        pass
+
+    def search_book_from_title(self):
+        pass
